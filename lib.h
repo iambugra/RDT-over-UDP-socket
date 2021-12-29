5a3b3e7d3cdfc8054807892f3d86c25f9e05d255
Buğra Alparslan <balparslan.58@gmail.com>
2021-12-29 20:44:23 +0300

    make_pkt() moved here

--- lib.h
+++ lib.h
@@ -51,22 +51,50 @@ typedef struct {
     bool isACK;
     int number;
     bool last_chunk;
-    bool valid;
+    bool avail;
+    bool sent;
     char payload[8];
 } Packet;
 
 
 
-int compute_cheksum(bool isACK, int number, bool last_chunk, bool valid, char *payload){
+int compute_cheksum(bool isACK, int number, bool last_chunk, bool avail, bool sent, char *payload){
     
     int res = 0;
 
-    res += (isACK + number + last_chunk + valid);
+    res += (isACK + number + last_chunk + avail + sent);
 
     for (int i=0; i<8; i++)
         res += payload[i];
 
     return res;
+}
+
+
+Packet make_pkt(int number, bool ACK, bool last, bool avail, bool sent, string msg){
+    Packet datagram;
+
+    datagram.avail = avail;
+    datagram.isACK = ACK;
+    datagram.sent = sent;
+
+    // mutex possible
+    datagram.number = number;
+
+    datagram.last_chunk = last;
+    
+    memset(datagram.payload, 0, sizeof(char) * 8);
+
+    int idx = 0;
+    for (idx=0; idx<msg.length(); idx++)
+        datagram.payload[idx] = msg[idx];
+
+    for (; idx<8; idx++)
+        datagram.payload[idx] = '\0';
+
+    datagram.checksum = compute_cheksum(datagram.isACK, datagram.number, datagram.last_chunk, datagram.avail, datagram.sent, datagram.payload);
+
+    return datagram;
 }
 
 
